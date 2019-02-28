a318c60cfaed0671a63ff8e78c49af8d8da87985
Magnus Thor <magnus.thor.ar@gmail.com>
2019-02-28 14:49:25 +0100

    adds feature test user can get cooper calculation result

---
+++ src/__features__/UserCanGetCooperCalculationResult.feature.js
@@ -0,0 +1,33 @@
+describe("Cooper Client", async () => {
+  beforeAll(async () => {
+    await page.goto("http://localhost:3001");
+  });
+
+  beforeEach(async () => {
+    await page.reload();
+  });
+
+  describe("calculates successfully", async () => {
+    beforeEach(async () => {
+      await page.type('input[id="distance"]', "1000");
+      await page.select('select[id="gender"]', "female");
+      await page.type('input[id="age"]', "23");
+    });
+
+    it("displays age", async () => {
+      await expect(page).toMatch("23 y/o");
+    });
+
+    it("displays gender", async () => {
+      await expect(page).toMatch("female");
+    });
+
+    it("displays distance", async () => {
+      await expect(page).toMatch("running 1000 meters");
+    });
+
+    it("displays result", async () => {
+      await expect(page).toMatch("Result: Poor");
+    });
+  });
+});
