d5711bc2ae3aabcd64c5014a2dba40d5bcfc0fd9
Magnus Thor <magnus.thor.ar@gmail.com>
2019-03-01 14:22:49 +0100

    adds cooper calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          <label>Distance</label>
+          <input id="distance" />
+        </div>
+
+        <select id="gender">
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+        </select>
+
+        <div>
+          <label>Age</label>
+          <input id="age" />
+        </div>
       </div>
     );
   }
