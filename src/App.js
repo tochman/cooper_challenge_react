194a0122e9936c4ab655c0a4ab80f7619293cf9b
Magnus Thor <magnus.thor.ar@gmail.com>
2019-03-01 14:25:49 +0100

    displays results from cooper calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from "react";
 import "./App.css";
+import DisplayCooperResult from "./Components/DisplayCooperResult";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      distance: "",
+      gender: "female",
+      age: ""
+    };
+  }
+
+  onChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
   render() {
     return (
       <div>
         <div>
           <label>Distance</label>
-          <input id="distance" />
+          <input id="distance" onChange={this.onChange.bind(this)} />
         </div>
 
-        <select id="gender">
+        <select id="gender" onChange={this.onChange.bind(this)}>
           <option value="female">Female</option>
           <option value="male">Male</option>
         </select>
 
         <div>
           <label>Age</label>
-          <input id="age" />
+          <input id="age" onChange={this.onChange.bind(this)} />
         </div>
+
+        <DisplayCooperResult
+          distance={this.state.distance}
+          gender={this.state.gender}
+          age={this.state.age}
+        />
       </div>
     );
   }
