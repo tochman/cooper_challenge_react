13e41e2ec8a5460fe19f993a8f2e37c170ef1ea5
Greg <gerg.kallai@gmail.com>
2019-03-15 11:54:50 +0100

    [WIP] Adds test for show old results
    Co-authored-by: Aditya Naik <naik.aditya@gmail.com>

--- cypress/integration/user_can_save_data.spec.js
+++ cypress/integration/user_can_save_data.spec.js
@@ -1,12 +1,26 @@
 describe('User can save data', () => {
     it('User logs in and input data', () => {
-      cy.login('user@mail.com', 'password');
-      cy.get('input[placeholder=Distance]').type('1500');
-      cy.get('.ui > .dropdown').click();
-      cy.contains('Male').click();
-      cy.get('input[placeholder=Age]').type('24');
-      cy.contains('Result: Poor')
-      cy.contains('24 y/o male running 1500 meters.')
-      cy.contains('Save entry')
+        cy.login('user@mail.com', 'password');
+        cy.get('input[placeholder=Distance]').type('1500');
+        cy.get('.ui > .dropdown').click();
+        cy.contains('Male').click();
+        cy.get('input[placeholder=Age]').type('24');
+        cy.contains('Result: Poor')
+        cy.contains('24 y/o male running 1500 meters.')
+        cy.contains('Save entry')
     })
-  })
+    it('User can save entry', () => {
+        cy.server()
+        cy.route(
+            'POST',
+            'http://localhost:3000/api/v1/performance_data',
+            'fixture:save_entry.json'
+        )
+        cy.contains('Save entry').click();
+        cy.contains('Your entry was saved')
+    })
+    it('User can see past entries', () => {
+        cy.contains('Show past entries').click();
+        cy.contains('Hide past entries')
+    })
+})
