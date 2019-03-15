9cc1af2e2f48f2b872cdaf7cd4833cc57ca910b2
Greg <gerg.kallai@gmail.com>
2019-03-15 11:18:04 +0100

    Adds stub for login flow
    Co-authored-by: Aditya Naik <naik.aditya@gmail.com>

--- cypress/integration/user_can_login.spec.js
+++ cypress/integration/user_can_login.spec.js
@@ -3,6 +3,15 @@ describe('User can log in', () => {
     cy.visit('http://localhost:3001');
   })
   it('User can log in successfully', () => {
+    cy.server();
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/auth/sign_in',
+      response: 'fixture:login.json',
+      headers: {
+        "uid": "user@mail.com"
+      }
+    })
     // cy.contains('Login').click();
     cy.get('#login').click();
     cy.get('#login-form').within(() => {
@@ -10,6 +19,7 @@ describe('User can log in', () => {
       cy.get('#password').type('password')
       cy.get('button').click()
     })
+    cy.contains('Hi user@mail.com')
     // cy.get('#login-form').as('loginform');
     // cy.get('@loginform').find("input[placeholder='E-mail address']").type('user@mail.com')
     // cy.get('@loginform').find("input[placeholder='Password']").type('password')
